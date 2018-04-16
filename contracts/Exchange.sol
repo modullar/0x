03438371415e1e35e5f98b346d84d083edf7c468
Mouaz Al-Qudsi <mouaz@gmail.com>
2018-04-16 22:46:46 +0200

    simple exchange scenario with testing

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -15,7 +15,6 @@ contract Exchange {
 
   Bid[] public bids;
 
-
   uint256 storedData;
 
 
@@ -30,34 +29,77 @@ contract Exchange {
     return relayer;
   }
 
+  function getMaker() public returns (address){
+    return maker;
+  }
+
+  function getTaker() public returns (address){
+    return taker;
+  }
+
+  function getBid(uint n) public returns(address, address, address, string, bytes32){
+    return(bids[n].maker, bids[n].taker, bids[n].relayer, bids[n].status, stringToBytes32(bids[n].status));
+  }
+
+  function bidsCount() public returns (uint256) {
+    return bids.length;
+  }
+
   function createBid() public returns (bool){
-    if(msg.sender == maker){
-      Bid memory bid = Bid({maker: msg.sender, taker: msg.sender, relayer: msg.sender, status: 'created'});
+    // To be able to test
+    doCreateBid(msg.sender);
+  }
+
+  function doCreateBid(address _sender)public returns (bool){
+    if(_sender == maker){
+      Bid memory bid = Bid({maker: _sender, taker: _sender, relayer: _sender, status: 'created'});
       bids.push(bid);
       return true;
     }
     return false;
   }
 
-  function PostBid() public returns (bool) {
-    return true;
+  function PostBid(uint n) public returns (bool) {
+    doPostBid(msg.sender, n);
   }
 
-  function InterceptBid() public returns (bool) {
-    return true;
-
+  function doPostBid(address _sender, uint n) returns (bool){
+    var (m, t, r, s, b) = getBid(n);
+    if(_sender == relayer && compareStrings(s, 'created')){
+      bids[n].status = 'posted';
+      bids[n].relayer = relayer;
+      return true;
+    }
+    return false;
   }
 
-  function closeDeal() public returns(bool) {
-    return true;
-
+  function InterceptBid(uint n) public returns (bool) {
+    doInterceptBid(msg.sender, n);
   }
 
-  function set(uint256 data) public {
-    storedData = data;
+  function doInterceptBid(address _sender, uint n) returns (bool){
+    var (m, t, r, s, b) = getBid(n);
+    if(_sender == taker && compareStrings(s, 'posted')){
+      bids[n].status = 'intercepted';
+      bids[n].taker = taker;
+      return true;
+    }
+    return false;
   }
 
-  function get() public constant returns (uint256){
-    return storedData;
+  function compareStrings(string a, string b) view returns (bool){
+    return keccak256(a) == keccak256(b);
   }
+
+  function stringToBytes32(string memory source) returns (bytes32 result) {
+      bytes memory tempEmptyStringTest = bytes(source);
+      if (tempEmptyStringTest.length == 0) {
+          return 0x0;
+      }
+      assembly {
+          result := mload(add(source, 32))
+      }
+  }
+
+
 }
--- test/TestExchange.sol
+++ test/TestExchange.sol
@@ -5,9 +5,54 @@ import "truffle/DeployedAddresses.sol";
 import "../contracts/Exchange.sol";
 
 contract TestExchange{
+
+
+  address relayer;
+  address maker;
+  address taker;
+
+  Exchange exchange;
+
+  function beforeEach() {
+    maker = 0x3b11c3517b601b4cf6caf2112ca0acff1d25f36a;
+    taker = 0x3b11c3517b601b4cf6caf2112ca0acff1d25f36b;
+    relayer = 0x3b11c3517b601b4cf6caf2112ca0acff1d25f36c;
+    exchange = new Exchange(maker, taker, relayer);
+  }
+
+  function testInitExchange(){
+    Assert.equal(exchange.getRelayer(), relayer, "Relayer should be set");
+    Assert.equal(exchange.getTaker(), taker, "Relayer should be set");
+    Assert.equal(exchange.getMaker(), maker, "Relayer should be set");
+  }
+
   function testCreateBid(){
-    Exchange exchange = new Exchange(0x3b11c3517b601b4cf6caf2112ca0acff1d25f36a, 0x3b11c3517b601b4cf6caf2112ca0acff1d25f36a, 0x3b11c3517b601b4cf6caf2112ca0acff1d25f36b);
-    address expected = 0x3b11c3517b601b4cf6caf2112ca0acff1d25f36b;
-    Assert.equal(exchange.getRelayer(), expected, "Relayer should be set");
+    Assert.equal(exchange.doCreateBid(maker), true, "Maker could create a bid");
+    Assert.equal(exchange.bidsCount(), 1, "A new bid will be created");
+    var (m, t, r, s, b) = (exchange.getBid(0));
+    Assert.equal(m, maker, "the right maker should be set");
+    Assert.equal(r, maker, "the bid still has no relayer");
+    Assert.equal(t, maker, "the bid still has no taker");
+    Assert.equal(b, bytes32('created'), "the right status should be set");
+    Assert.equal(exchange.doCreateBid(taker), false, "Taker could not create a bid");
+  }
+
+  function testPostBid(){
+    exchange.doCreateBid(maker);
+    Assert.equal(exchange.doPostBid(maker, 0), false, "Bid cannot be posted by maker");
+    Assert.equal(exchange.doPostBid(relayer, 0), true, "Bid will be posted by the relayer");
+    var (m, t, r, s, b) = (exchange.getBid(0));
+    Assert.equal(b, bytes32('posted'), "Bid will be posted by the relayer");
+    Assert.equal(r, relayer, "Assign relayer to the posted bid");
+  }
+
+  function testInterceptBid(){
+    exchange.doCreateBid(maker);
+    Assert.equal(exchange.doInterceptBid(taker, 0), false, "Cannot intercept unposted Bid");
+    exchange.doPostBid(relayer, 0);
+    Assert.equal(exchange.doInterceptBid(taker, 0), true, "Bid is intercepted");
+    var (m, t, r, s, b) = (exchange.getBid(0));
+    Assert.equal(b, bytes32('intercepted'), "Intercepted status is changed");
+    Assert.equal(t, taker , "taker was assigned");
   }
 }
